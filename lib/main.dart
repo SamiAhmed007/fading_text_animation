3e4fcab512b389dc77e2acb5ecbc0cb859ce1c3f
Mohammed Sami Ahmed <mohammedsamiahmed@Mohammeds-MacBook-Pro.local>
2024-10-03 18:57:53 -0400

    Added image with rounded corners and visibility toggle final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 void main() {
   runApp(MyApp());
@@ -18,10 +19,23 @@ class FadingTextAnimation extends StatefulWidget {
   _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 
-class _FadingTextAnimationState extends State<FadingTextAnimation> {
+class _FadingTextAnimationState extends State<FadingTextAnimation>
+    with SingleTickerProviderStateMixin {
   bool _isVisible = true;
   bool _showFrame = false;
-  bool _showImage = true; // New boolean to control image visibility
+  bool _showImage = true;
+  bool _rotateImage = false;
+
+  late AnimationController _rotationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _rotationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(); // Rotates continuously
+  }
 
   void toggleVisibility() {
     setState(() {
@@ -41,6 +55,23 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     });
   }
 
+  void toggleRotation() {
+    setState(() {
+      _rotateImage = !_rotateImage;
+      if (_rotateImage) {
+        _rotationController.repeat();
+      } else {
+        _rotationController.stop();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _rotationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,6 +87,7 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
               child: AnimatedOpacity(
                 opacity: _isVisible ? 1.0 : 0.0,
                 duration: const Duration(seconds: 1),
+                curve: Curves.easeInOut,
                 child: Container(
                   padding: const EdgeInsets.all(8.0),
                   decoration: _showFrame
@@ -73,37 +105,62 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
             ),
             const SizedBox(height: 20),
             _showImage
-                ? Container(
-                    width: 150,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      image: DecorationImage(
-                        image: AssetImage('assets/img2.png'),
-                        fit: BoxFit.cover,
+                ? AnimatedBuilder(
+                    animation: _rotationController,
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        image: DecorationImage(
+                          image: AssetImage('assets/img2.png'),
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
+                    builder: (context, child) {
+                      return Transform.rotate(
+                        angle: _rotateImage
+                            ? _rotationController.value * 2 * math.pi
+                            : 0,
+                        child: child,
+                      );
+                    },
                   )
                 : Container(),
           ],
         ),
       ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SwitchListTile(
-            title: Text('Show Frame'),
-            value: _showFrame,
-            onChanged: toggleFrame,
-          ),
-          SwitchListTile(
-            title: Text('Show Image'),
-            value: _showImage,
-            onChanged: (bool value) {
-              toggleImageVisibility();
-            },
-          ),
-        ],
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SwitchListTile(
+              title: Text('Show Frame'),
+              value: _showFrame,
+              onChanged: toggleFrame,
+            ),
+            SwitchListTile(
+              title: Text('Show Image'),
+              value: _showImage,
+              onChanged: (bool value) {
+                toggleImageVisibility();
+              },
+            ),
+            SwitchListTile(
+              title: Text('Rotate Image'),
+              value: _rotateImage,
+              onChanged: (bool value) {
+                toggleRotation();
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
